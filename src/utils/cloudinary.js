97ce40b23dfde89de098b53e4428c9762633bf35
aryan <aryantomar6200@gmail.com>
2026-08-07 00:06:36 +0530

    complete project setup - cloudinary multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,37 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+
+        //uploading on cloudinary from our server
+        const uploadResult = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+
+        console.log("cloudinary file uploaded: uploadResult ", uploadResult);
+        return uploadResult
+        
+    } catch (error) {
+        fs.unlink(localFilePath) //removing temp file from our server
+        return null
+    }
+}
+
+
+const uploadResult = await cloudinary.uploader
+       .upload(
+           'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+               public_id: 'shoes',
+           }
+       )
+       .catch((error) => {
+           console.log(error);
+       });
