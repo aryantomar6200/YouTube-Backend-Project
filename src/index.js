fc4266e98c3596d660ddb0054e1aef6470b72377
aryan <aryantomar6200@gmail.com>
2026-08-06 12:53:33 +0530

    add utils code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
-import dotenv from "dotenv"
-import connectDB from "./db/index.js"
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-dotenv.config()
+dotenv.config();
+
 connectDB()
+  .then(() => {
+    app.on("on_errorrr!!", (err) => {
+      console.log(err);
+      throw err;
+    });
+
+    app.listen(process.env.PORT || 3000, () => {
+      console.log("app is running on port: ", process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log("DB connection failed !!!", err);
+  });
